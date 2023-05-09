66f09aacc63b63200b3a0986d2c232f8a74175bd
Louise BERTIN <louise@trustfolio.co>
2023-05-09 14:46:30 +0200

    Project ready

---
+++ front/src/test/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+    let name = 'test23';
+
+    let list = ['a', 'b', 'c'];
+    
+</script>
+
+<h1>Test page : {name}</h1>
+
+<ul>
+    {#each list as l}
+    <li>
+        <button on:click={() => name = l}>
+            {l}
+        </button>
+    </li>
+    {/each}
+</ul>
